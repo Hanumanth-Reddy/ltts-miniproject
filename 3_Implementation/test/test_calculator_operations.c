b1a62be28e52fed42a82a9c662e323c82212894f
Hanumanth <bodanapu94@gmail.com>
2021-04-10 22:19:24 +0530

    added unity test cases

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -3,6 +3,8 @@
 #define PROJECT_NAME    "Calculator"
 calculate testcal={10,5};
 trigonometric testo={4};
+calculate testcal1={3,2};
+trigonometric testo1={6};
 /* Prototypes for all the test functions */
 void test_sum(void);
 void test_substraction(void);
@@ -51,43 +53,57 @@ int main()
 /* Write all the test functions */ 
 void test_sum(void) {
   TEST_ASSERT_EQUAL(15,sum(&testcal));
+   TEST_ASSERT_EQUAL(5,sum(&testcal1));
 }
 void test_substraction(void) {
   TEST_ASSERT_EQUAL(5,substraction(&testcal));
+  TEST_ASSERT_EQUAL(1,substraction(&testcal1));
 }
 void test_multiplication(void) {
   TEST_ASSERT_EQUAL(50,multiplication(&testcal));
+  TEST_ASSERT_EQUAL(6,multiplication(&testcal1));
 }
 void test_division(void) {
   TEST_ASSERT_EQUAL(2,division(&testcal));
+  TEST_ASSERT_EQUAL(1,division(&testcal1));
 }
 void test_power(void) {
   TEST_ASSERT_EQUAL(100000,power(&testcal));
+   TEST_ASSERT_EQUAL(9,power(&testcal1));
 }
 void test_root(void) {
   TEST_ASSERT_EQUAL(2,squareroot(&testo));
+  TEST_ASSERT_EQUAL(2.449,squareroot(&testo1));
 }
 void test_factorial(void) {
   TEST_ASSERT_EQUAL(24,factorial(&testo));
+  TEST_ASSERT_EQUAL(720,factorial(&testo1));
 }
 void test_exponent(void) {
   TEST_ASSERT_EQUAL(54.598,exponent(&testo));
+   TEST_ASSERT_EQUAL(403.428,exponent(&testo1));
 }
 void test_sine(void) {
   TEST_ASSERT_EQUAL(-0.756,sine(&testo));
+  TEST_ASSERT_EQUAL(-0.279,sine(&testo1));
 }
 void test_cosine(void) {
   TEST_ASSERT_EQUAL(-0.653,cosine(&testo));
+  TEST_ASSERT_EQUAL(-0.960,cosine(&testo1));
 }
 void test_ttan(void) {
   TEST_ASSERT_EQUAL(1.157,ttan(&testo));
+   TEST_ASSERT_EQUAL(-0.291,ttan(&testo1));
 }
 void test_sec(void) {
   TEST_ASSERT_EQUAL(-1.529,sec(&testo));
+   TEST_ASSERT_EQUAL(1.414,sec(&testo1));
 }
 void test_cot(void) {
   TEST_ASSERT_EQUAL(0.863,cot(&testo));
+  TEST_ASSERT_EQUAL(-3.463,cot(&testo1));
 }
 void test_cosec(void) {
   TEST_ASSERT_EQUAL(-1.321,cosec(&testo));
+   TEST_ASSERT_EQUAL(-3.578,cosec(&testo1));
 }
