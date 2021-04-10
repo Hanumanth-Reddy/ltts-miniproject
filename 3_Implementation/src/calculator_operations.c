da904eb4d87f6d28541a137296dd2a849921c412
Hanumanth <bodanapu94@gmail.com>
2021-04-10 22:01:55 +0530

    added unity test cases

--- 3_Implementation/src/calculator_operations.c
+++ 3_Implementation/src/calculator_operations.c
@@ -2,53 +2,66 @@
 int sum(calculate *compute){
    int result=compute->value1+compute->value2;
    printf("%d + %d = %d\n",compute->value1,compute->value2,result);
+   return result;
 }
 int substraction(calculate *compute){
     int result=compute->value1-compute->value2;
     printf("%d - %d = %d\n",compute->value1,compute->value2,result);
+    return result;
 }
 int multiplication(calculate *compute){
     int result=compute->value1*compute->value2;
-     printf("%d - %d = %d\n",compute->value1,compute->value2,result);}
+     printf("%d * %d = %d\n",compute->value1,compute->value2,result);
+     return result;}
 int division(calculate *compute){
     int result= compute->value1/compute->value2;
       printf(" quotient when %d / %d = %d\n",compute->value1,compute->value2,result);
-      printf(" remainder when %d / %d = %d\n",compute->value1,compute->value2,compute->value1%compute->value2);}
+      printf(" remainder when %d / %d = %d\n",compute->value1,compute->value2,compute->value1%compute->value2);
+      return result;}
 int squareroot(trigonometric *tri){
      int result=sqrt(tri->angle);
      printf("squareroot of %d = %.3f\n",tri->angle,result);
+     return result;
 }
 int power(calculate *compute){
 int result= pow(compute->value1,compute->value2);
  printf("%d ^%d = %d\n",compute->value1,compute->value2,result);
+ return result;
 }
 int sine(trigonometric *tri){
 float result= sin(tri->angle);
  printf("sin %d = %.3f\n",tri->angle,result);
+ return result;
 }
 int cosine(trigonometric *tri){
 float result=cos(tri->angle);
  printf("cos %d = %.3f\n",tri->angle,result);
+  return result;
 }
 int ttan(trigonometric *tri){
 float result= tan(tri->angle);
  printf("tan %d = %.3f\n",tri->angle,result);
+ return result;
 }
 int cosec(trigonometric *tri){
 float result=1/sin(tri->angle);
  printf("cosec %d = %.3f\n",tri->angle,result);
+ return result;
 }
 int sec(trigonometric *tri){
 float result=1/cos(tri->angle);
  printf("sec %d = %.3f\n",tri->angle,result);
+ return result;
 }
 int cot(trigonometric *tri){
 float result= 1/tan(tri->angle);
  printf("cot %d = %.3f\n",tri->angle,result);
+ return result;
 }
 int exponent(trigonometric *tri){
 float result= exp(tri->angle);
  printf("e ^%d = %.3f\n",tri->angle,result);
+ return result;
 }
 int factorial(trigonometric *tri){
     long long int fact=1;
@@ -58,4 +71,5 @@ int factorial(trigonometric *tri){
             fact *= i;
         }
  printf("%d!= %lld\n",tri->angle,fact);
+ return fact;
 }
--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -1,17 +1,23 @@
 #include "unity.h"
-#include <calculator_operations.h>
-
-/* Modify these two lines according to the project */
-#include <calculator_operations.h>
+#include "calculator_operations.h"
 #define PROJECT_NAME    "Calculator"
-
+calculate testcal={10,5};
+trigonometric testo={4};
 /* Prototypes for all the test functions */
-void test_add(void);
-void test_add_testcase2(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
-
+void test_sum(void);
+void test_substraction(void);
+void test_multiplication(void);
+void test_division(void);
+void test_exponent(void);
+void test_factorial(void);
+void test_root(void);
+void test_power(void);
+void test_sine(void);
+void test_cosine(void);
+void test_ttan(void);
+void test_sec(void);
+void test_cosec(void);
+void test_cot(void);
 /* Required by the unity test framework */
 void setUp(){}
 /* Required by the unity test framework */
@@ -24,45 +30,64 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_add);
-  RUN_TEST(test_add_testcase2);
-  RUN_TEST(test_subtract);
-  RUN_TEST(test_multiply);
-  RUN_TEST(test_divide);
-
+  RUN_TEST(test_sum);
+  RUN_TEST(test_substraction);
+  RUN_TEST(test_multiplication);
+  RUN_TEST(test_division);
+  RUN_TEST(test_root);
+  RUN_TEST(test_power);
+  RUN_TEST(test_exponent);
+  RUN_TEST(test_factorial);
+  RUN_TEST(test_sine);
+  RUN_TEST(test_cosine);
+  RUN_TEST(test_ttan);
+  RUN_TEST(test_cosec);
+  RUN_TEST(test_sec);
+  RUN_TEST(test_cot);
   /* Close the Unity Test Framework */
   return UNITY_END();
 }
 
 /* Write all the test functions */ 
-void test_add(void) {
-  TEST_ASSERT_EQUAL(30, add(10, 20));
-  TEST_ASSERT_EQUAL(-10, add(10, -20));
-  TEST_ASSERT_EQUAL(-30, add(-10, -20));
-  TEST_ASSERT_EQUAL(10, add(-10, 20));
+void test_sum(void) {
+  TEST_ASSERT_EQUAL(15,sum(&testcal));
 }
-void test_add_testcase2(void) {
- 
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(1500, add(750, 7500));
+void test_substraction(void) {
+  TEST_ASSERT_EQUAL(5,substraction(&testcal));
 }
-void test_subtract(void) {
-  TEST_ASSERT_EQUAL(-3, subtract(0, 3));
-  
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(1, subtract(1000, 900));
+void test_multiplication(void) {
+  TEST_ASSERT_EQUAL(50,multiplication(&testcal));
 }
-
-void test_multiply(void) {
-  TEST_ASSERT_EQUAL(0, multiply(1, 0));
-  
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(2, multiply(2, 5));
+void test_division(void) {
+  TEST_ASSERT_EQUAL(2,division(&testcal));
 }
-
-void test_divide(void) {
-  TEST_ASSERT_EQUAL(0, divide(1, 0));
-  
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(3, divide(2, 2));
+void test_power(void) {
+  TEST_ASSERT_EQUAL(100000,power(&testcal));
+}
+void test_root(void) {
+  TEST_ASSERT_EQUAL(2,squareroot(&testo));
+}
+void test_factorial(void) {
+  TEST_ASSERT_EQUAL(24,factorial(&testo));
+}
+void test_exponent(void) {
+  TEST_ASSERT_EQUAL(54.598,exponent(&testo));
+}
+void test_sine(void) {
+  TEST_ASSERT_EQUAL(-0.756,sine(&testo));
+}
+void test_cosine(void) {
+  TEST_ASSERT_EQUAL(-0.653,cosine(&testo));
+}
+void test_ttan(void) {
+  TEST_ASSERT_EQUAL(1.157,ttan(&testo));
+}
+void test_sec(void) {
+  TEST_ASSERT_EQUAL(-1.529,sec(&testo));
+}
+void test_cot(void) {
+  TEST_ASSERT_EQUAL(0.863,cot(&testo));
+}
+void test_cosec(void) {
+  TEST_ASSERT_EQUAL(-1.321,cosec(&testo));
 }
